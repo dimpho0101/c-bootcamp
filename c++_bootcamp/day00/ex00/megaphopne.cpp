0fa1729e265a543646b093c51749c1bc517f3407
kudakwashe siziva <9620622+kaysiz@users.noreply.github.com>
2019-06-05 10:59:11 +0200

    Update megaphopne.cpp

--- c++_bootcamp/day00/ex00/megaphopne.cpp
+++ c++_bootcamp/day00/ex00/megaphopne.cpp
@@ -1,28 +1,28 @@
 #include <iostream>
 #include <string>
-#include <stdio.h>
-#include <ctype.h>
-#include <cstring>
-#include <cstdio>
 
 int main(int ac, char **av)
 {
 	int i;
-   	i = 1;
-	if (ac > 1)
-	{
-		int x = 0;
-		while (av[i][x])
+  i = 1;
+
+		while (i < ac)
 		{
+			int x = 0;
 			//std:: cout << &av[1][i];
-			std::cout << char(toupper(av[i][x++]));
+			while (av[i][x])
+      {
+				std::cout << char(toupper(av[i][x++]));
+      }
+      i++;
 		}
-	}
+	if (ac == 1)
+    std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
 	//std::string mystring;
 	//mystring = &av[1][i];
 	//std::cout << char(toupper(mystring));
 	//i++;
-	//std::cout << mystring;
+	std::cout << std::endl;
 
 	return 0;
 
